78f6519a9ce4be371a0f4e5a13176c302b02c057
PS-Soham-Vaghela <soham.vaghela@productsquads.co>
2026-07-15 14:47:20 +0530

    Exercise 12 : Create film_catalog view and query for comedies
    Created a view for the film catalog and added a query to retrieve comedy films.

--- Exercise 12 A Reusable Film Catalog.sql
+++ Exercise 12 A Reusable Film Catalog.sql
@@ -1,3 +1,4 @@
+--Step 1 Create the view
 CREATE VIEW film_catalog AS
 SELECT
     f.title,
@@ -9,3 +10,12 @@ JOIN film_category fc
     ON f.film_id = fc.film_id
 JOIN category c
     ON fc.category_id = c.category_id;
+
+--Step 2 Query the view
+SELECT
+    title,
+    rental_rate,
+    length
+FROM film_catalog
+WHERE category = 'Comedy'
+ORDER BY title;
