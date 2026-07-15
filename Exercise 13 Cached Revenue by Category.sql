0248f4de5fde9d63e56fb8dfabb037bfc25d6a9a
PS-Soham-Vaghela <soham.vaghela@productsquads.co>
2026-07-15 16:09:53 +0530

    Exercise 13 Create materialized view for category revenue added
    Added a query to retrieve the top 5 categories by revenue from the materialized view.

---
+++ Exercise 13 Cached Revenue by Category.sql
@@ -0,0 +1,23 @@
+--Create Maerialized view
+CREATE MATERIALIZED VIEW category_revenue AS
+SELECT
+    c.name AS category,
+    SUM(p.amount) AS revenue
+FROM payment p
+INNER JOIN rental r
+    ON p.rental_id = r.rental_id
+INNER JOIN inventory i
+    ON r.inventory_id = i.inventory_id
+INNER JOIN film_category fc
+    ON i.film_id = fc.film_id
+INNER JOIN category c
+    ON fc.category_id = c.category_id
+GROUP BY c.name;
+
+--Query the Materilized view
+SELECT
+    category,
+    revenue
+FROM category_revenue
+ORDER BY revenue DESC
+LIMIT 5;
